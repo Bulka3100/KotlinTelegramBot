0db9acfb4e57675eefc705f27e50b7f3ab77e2ec
Breaker <kirillvip3100@yandex.ru>
2025-01-25 19:20:35 +0300

    KTB-02-File-created-refactor

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -3,7 +3,6 @@ package org.example
 import java.io.File
 
 fun main() {
- val wordsFile = File("words.txt")
- wordsFile.createNewFile() //в файл же вручную записали значения
- for (i in wordsFile.readLines()) println(i)
+    val wordsFile = File("words.txt")
+    wordsFile.forEachLine { line -> println(line) }
 }
