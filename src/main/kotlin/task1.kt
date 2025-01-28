f60bc2bdb2d6c35b82b54ab28d8eb83b0764ff0c
Breaker <kirillvip3100@yandex.ru>
2025-01-29 02:33:32 +0300

    KTB-02-dictionary-refactor

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -4,14 +4,17 @@ import java.io.File
 
 fun main() {
     val wordsFile = File("words.txt")
-    val dictionary=mutableListOf<Word>()
-    wordsFile.forEachLine { line -> val line=line.split("|");
-        val word= Word( origin = line[0], translate = line[1], correctAnswerCount = line[2].toInt() ?: 0)
-    dictionary.add(word)}
-println(dictionary)
+    val dictionary = mutableListOf<Word>()
+    wordsFile.forEachLine { line ->
+        val line = line.split("|")
+        val word = Word(origin = line[0], translate = line[1], correctAnswerCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
+        dictionary.add(word)
+    }
+    println(dictionary)
 }
+
 data class Word(
     val correctAnswerCount: Int = 0,
     val origin: String,
     val translate: String,
-    )
+)
