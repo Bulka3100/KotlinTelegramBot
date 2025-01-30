843f8b512ed87d649c98ff44b3867fbda5f32993
Breaker <kirillvip3100@yandex.ru>
2025-01-30 15:05:04 +0300

    KTB-04-done

--- src/main/kotlin/task2.kt
+++ src/main/kotlin/task2.kt
@@ -1,20 +1,38 @@
-fun main() {
+import java.io.File
 
-while (true){
-    println("""Меню:
+fun main() {
+    val wordsFile = File("words.txt")
+    val dictionary = mutableListOf<Word>()
+    fun loadDictionary(): MutableList<Word> {
+        wordsFile.forEachLine { line ->
+            val line = line.split("|")
+            val word =
+                Word(origin = line[0], translate = line[1], correctAnswerCount = line.getOrNull(2)?.toIntOrNull() ?: 0)
+            dictionary.add(word)
+        }
+        return dictionary
+    }
+
+    while (true) {
+        println(
+            """Меню:
         |1-Учить слова
         |2-Статистика
         |0-Выход
-    """.trimMargin())
-    val choice = readln()
-    when(choice){
-        "1"->println("Учить слова")
-        "2"-> println("Статистика")
-        "0"->break
-        else -> println("введите пункт меню")
+    """.trimMargin()
+        )
+        val choice = readln()
+        when (choice) {
+            "1" -> println("Учить слова")
+            "2" -> println("Статистика")
+            "0" -> break
+            else -> println("введите пункт меню")
+        }
     }
 }
-}
-fun loadDictionary(){
 
-}
+data class Word(
+    val correctAnswerCount: Int = 0,
+    val origin: String,
+    val translate: String,
+)
