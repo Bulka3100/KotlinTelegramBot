721f3936755835262df57a3777a5313bbf91c7fd
Breaker <kirillvip3100@yandex.ru>
2025-03-02 02:19:27 +0300

    KTB-10

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -9,17 +9,32 @@ import java.net.http.HttpResponse
 fun main(args: Array<String>) {
     val botToken = args[0]
     val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    var updateId = 0
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val requestGet: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
+    val responseGet: HttpResponse<String> = client.send(requestGet, HttpResponse.BodyHandlers.ofString())
+    println(responseGet.body())
+
+    while (true) {
+        Thread.sleep(2000)
+        val updates: String = getUpdates(botToken, updateId)
+        println(updates)
+        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+        val startUpdateId = updates.lastIndexOf("update_id") + 11
+        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+        if(startUpdateId ==-1 || endUpdateId ==-1) continue
+        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+      //  println(updateIdString)
+        updateId = updateIdString.toInt() + 1
+    }
+}
+
+fun getUpdates(botToken: String, updateId: Int): String {
+    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
 
     val client: HttpClient = HttpClient.newBuilder().build()
 
-    val requestGet: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
     val requestUpdate: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
-
-
-    val responseGet: HttpResponse<String> =client.send(requestGet, HttpResponse.BodyHandlers.ofString())
-    val responseUpdate: HttpResponse<String> =client.send(requestUpdate, HttpResponse.BodyHandlers.ofString())
-
-    println(responseGet.body())
-    println(responseUpdate.body())
+    val responseUpdate: HttpResponse<String> = client.send(requestUpdate, HttpResponse.BodyHandlers.ofString())
+    return responseUpdate.body()
 }
