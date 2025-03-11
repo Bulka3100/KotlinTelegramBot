49a376d07504d50ab5f5f5fdecec4d1a11d88892
Breaker <kirillvip3100@yandex.ru>
2025-03-12 02:56:46 +0300

    KTB-14

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -59,6 +59,67 @@ class TelegramBotService(
         val httpRequest: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         client.send(httpRequest, HttpResponse.BodyHandlers.ofString())
     }
+
+    fun sendQuestion(chatId: Long?, question: Question?) {
+        val urlSendMessage = "${org.example.BASE_API}$tokenBot/sendMessage"
+        if (question != null) {
+            val sendQuestionBody = """
+            {
+                "chat_id": $chatId,
+                "text": "${question?.correctAnswer?.origin}",
+                "reply_markup":{
+                    "inline_keyboard":[
+                        [
+                            {
+                                "text":"${question.variants.get(0).translate}",
+                                "callback_data":"${CALLBACK_DATA_ANSWER_PREFIX}0"
+                            },
+                             {
+                                "text":"${question.variants.get(1).translate}",
+                                "callback_data":"${CALLBACK_DATA_ANSWER_PREFIX}1"
+                            }
+                        ],
+                        [
+                            {
+                                "text":"${question.variants.get(2).translate}",
+                                "callback_data":"${CALLBACK_DATA_ANSWER_PREFIX}2"
+                            },
+                             {
+                                "text":"${question.variants.get(3).translate}",
+                                "callback_data":"${CALLBACK_DATA_ANSWER_PREFIX}3"
+                            }
+                        ]
+                    ]
+                }
+
+            }
+        """.trimIndent()
+            val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage))
+                .header("Content-type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(sendQuestionBody))
+                .build()
+            client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        } else {
+            sendMessage(chatId, "Все слова выучены!")
+        }
+
+    }
+
+    fun checkNextQuestionAndSend(
+        trainer: LearnWordsTrainer,
+        telegramBotService: TelegramBotService,
+        chatId: Long?
+    ) {
+        val question = trainer.getNextQuestion()
+        if (question == null) {
+            telegramBotService.sendMessage(chatId, "Все слова в словаре выучены!")
+        } else {
+            telegramBotService.sendQuestion(chatId, question)
+        }
+    }
+
 }
 
 const val BASE_API = "https://api.telegram.org/bot"
+const val CALLBACK_DATA_ANSWER_PREFIX = "_answer"
